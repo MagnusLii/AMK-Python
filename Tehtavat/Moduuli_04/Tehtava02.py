3ae082ca90e1e81085a16f843012bfd6e25f9d56
Mage <magnus.liivamagi@gmail.com>
2022-08-24 13:46:09 +0300

    Lisätty teht 4.2.

---
+++ Tehtavat/Moduuli_04/Tehtava02.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        inch = float(input("Anna tuumien määrä: "))
+    except ValueError:
+        print("Syötä numero.")
+        continue
+    if inch <= 0:
+        print("Arvo on negatiivinen.\nExiting program.")
+        exit()
+    else:
+        cm = inch * 2.54
+        print(str(inch) +" tuuma on " +str(cm) +" cm.")
