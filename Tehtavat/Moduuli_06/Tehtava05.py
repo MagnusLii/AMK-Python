a2b5a46f901180a7fb8a27810d3b8ac5e7ffbdc2
Mage <magnus.liivamagi@gmail.com>
2022-08-29 01:39:30 +0300

    Updated 6.5

--- Tehtavat/Moduuli_06/Tehtava05.py
+++ Tehtavat/Moduuli_06/Tehtava05.py
@@ -4,7 +4,7 @@ full_list = []
 def removenum(list):
     templist = []
     for i in list:
-        if(i%2 == 0):
+        if i%2 == 0:
             templist.append(i)
     return templist
 
@@ -14,11 +14,11 @@ while True:
         nro = nro.lower()
         if nro == "exit":
             for i in full_list:
-                print(i,end=" ")
+                print(i, end=" ")
             print()
             data = removenum(full_list)
             for i in data:
-                print(i,end=" ")
+                print(i, end=" ")
             exit()
         else:
             nro = int(nro)
