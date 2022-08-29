473253c776956f90aead364cb9cb82e3de9aeca3
Mage <magnus.liivamagi@gmail.com>
2022-08-29 23:15:37 +0300

    Added 6.6

---
+++ Tehtavat/Moduuli_06/Tehtava06.py
@@ -0,0 +1,29 @@
+import math
+list = ["ekan", "toisen"]
+count = 0
+
+# biground = diameter, hunnie = price
+def pizzaNamNamCalc(biground, hunnie):
+    return hunnie/(math.pi*((biground/2)**2))
+
+while count < 2:
+    try:
+        priceperarea = pizzaNamNamCalc(float(input("Anna " +list[count] +" pizzan halkaisija senttimetreinä: ")),
+                                        float(input("Anna " +list[count] +" pizzan hinta: ")))
+        # Jos haluat tietää pizzan hinnan per neliömetri.
+        #print("Pizzan hinta per neliömetri on " + str(round(priceperarea,2)) + "€.")
+    except ValueError:
+        print("Syötä numero!")
+    else:
+        if count == 0:
+            price1 = str(round(priceperarea,2))
+        elif count == 1:
+            price2 = str(round(priceperarea,2))
+        count = count + 1
+
+if price1 == price2:
+    print("Molemilla pizzoilla on sama hinta per bite.")
+elif price1 < price2:
+    print("Ekalla pizzalla on parempi hinta.")
+else:
+    print("Toisella pizzalla on parempi hinta.")
