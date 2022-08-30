92162391ad046cca0e67eb09cfdce36f1781d75a
Mage <magnus.liivamagi@gmail.com>
2022-08-30 10:33:39 +0300

    Made code more readable.

--- Tehtavat/Moduuli_04/Tehtava06.py
+++ Tehtavat/Moduuli_04/Tehtava06.py
@@ -1,12 +1,12 @@
 import random
 
 # Var
-xcoord = []                     # Lists for storing all X/Y coordinates.
+xcoord = []                 # Lists for storing all X/Y coordinates.
 ycoord = []
-ind = 0                         # Var for keeping track of which item were reading on the list.
-number_of_entries = 0                 # Variable for storing the number of numbers asked to be generated.
-inside_of_circle = 0             # These two are variables used to keep track of the...
-outside_of_circle = 0        # ratio of numbers within and outside the circle.
+ind = 0                     # Var for keeping track of which item were reading on the list.
+number_of_entries = 0       # Variable for storing the number of numbers asked to be generated.
+inside_of_circle = 0        # These two are variables used to keep track of the...
+outside_of_circle = 0       # ratio of numbers within and outside the circle.
 
 # Functions
 def randcoord():
@@ -28,8 +28,8 @@ for i in range(number_of_entries):
 # Evaluating positions.
 for i in range(number_of_entries):
     if (xcoord[ind] ** 2) + (ycoord[ind] ** 2) < 1:
-        ind = ind + 1
         inside_of_circle = inside_of_circle + 1
+        ind = ind + 1
     else:
         outside_of_circle = outside_of_circle + 1
         ind = ind + 1
