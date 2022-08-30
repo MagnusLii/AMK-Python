5e274f54ce822a104f8b15e5ee824c9e02f8da45
Mage <magnus.liivamagi@gmail.com>
2022-08-30 09:49:57 +0300

    Trimmed code, fixed grammar.

--- Tehtavat/Moduuli_04/Tehtava03.py
+++ Tehtavat/Moduuli_04/Tehtava03.py
@@ -10,15 +10,17 @@ while True:
         input1 = float(input("Syötä numero: "))
     except ValueError:
         if input1 == "":    # Exits when user leaves input empty.
+            print("Exiting game.")
             break
         else:               # Gives error message and resets if user enters non numeric value.
             print("Error! Syötä vain numeroita.")
         continue
     else:                   # Appends user input to list and resets the variable to empty.
         numbers.append(input1)
-        input1 = ""
+        input1 = ""         # Needed for use to retain ability to close game.
 
 if len(numbers) == 0:       # Exits game without errors incase user enters no values into list.
+    print("Exiting game.")
     exit()
 
 # Extra var
@@ -29,14 +31,10 @@ smallest_num = numbers[0]
 for num in numbers:
     if num > largest_num:
         largest_num = num
-    else:
-        continue
 print("The biggest number is " +str(largest_num))
 
 # Finding smallest value.
 for num in numbers:
     if num < smallest_num:
         smallest_num = num
-    else:
-        continue
 print("The smallest number is " +str(smallest_num))
