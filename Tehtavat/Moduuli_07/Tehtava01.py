8e5e494f62a71197a58b617528d0944351aa49f0
Mage <magnus.liivamagi@gmail.com>
2022-08-30 13:48:57 +0300

    Added Module 7 and teht 1.

---
+++ Tehtavat/Moduuli_07/Tehtava01.py
@@ -0,0 +1,12 @@
+# kk = ("Joulukuu", "Tammikuu", "Helmikuu",
+#       "Maaliskuu", "Huhtikuu", "Toukokuu",
+#       "Kesäkuu", "Heinäkuu", "Elokuu",
+#       "Syyskuu", "Lokakuu", "Marraskuu")
+vuodenaika = ("Ei ole 0:tta kuukautta",
+              "Talvi", "Talvi","Kevät",
+              "kevät", "Kevät","Kesä",
+              "Kesä","Kesä","Syksy",
+              "Syksy","Syksy","Talvi")
+#int(input("Anna kk numero: "))
+print(vuodenaika[int(input("Anna kk numero: "))])
+
