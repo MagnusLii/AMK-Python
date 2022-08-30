5d7b16a6580fa9f9be80cfde63d6974fec92ba2c
Mage <magnus.liivamagi@gmail.com>
2022-08-30 09:40:58 +0300

    Fixed grammar.

--- Tehtavat/Moduuli_04/Tehtava02.py
+++ Tehtavat/Moduuli_04/Tehtava02.py
@@ -4,9 +4,9 @@ while True:
     except ValueError:
         print("Syötä numero.")
         continue
-    if inch <= 0:
+    if inch < 0:
         print("Arvo on negatiivinen.\nExiting program.")
         exit()
     else:
         cm = inch * 2.54
-        print(str(inch) +" tuuma on " +str(cm) +" cm.")
+        print(str(inch) +" tuuma on " +str(round(cm, 2)) +" cm.")
