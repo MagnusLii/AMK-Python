ad7d8d56e7048b6d7385222b8116a5c748035a89
Mage <magnus.liivamagi@gmail.com>
2022-08-31 16:33:50 +0300

    Cleaned up 4.6

--- Tehtavat/Moduuli_04/Tehtava06.py
+++ Tehtavat/Moduuli_04/Tehtava06.py
@@ -8,11 +8,6 @@ number_of_entries = 0       # Variable for storing the number of numbers asked t
 inside_of_circle = 0        # These two are variables used to keep track of the...
 outside_of_circle = 0       # ratio of numbers within and outside the circle.
 
-# Functions
-def randcoord():
-    xcoord.append(round(random.uniform(-1.001, 1.001),1))
-    ycoord.append(round(random.uniform(-1.001, 1.001),1))
-
 # Code
 # Number of coordinates generated.
 while number_of_entries == 0:
@@ -23,7 +18,8 @@ while number_of_entries == 0:
 
 # Generating coordinates.
 for i in range(number_of_entries):
-    randcoord()
+    xcoord.append(round(random.uniform(-1.003, 1.001), 1))
+    ycoord.append(round(random.uniform(-1.003, 1.001), 1))
 
 # Evaluating positions.
 for i in range(number_of_entries):
@@ -36,37 +32,6 @@ for i in range(number_of_entries):
 
 # Calculating pi
 try:
-    pi_approx = inside_of_circle / outside_of_circle
-    print(pi_approx)
+    print(inside_of_circle / outside_of_circle)
 except ZeroDivisionError:
     print("Kaikki pisteet osuivat ympyrän sisälle.")
-
-# Troubleshooting code.
-
-# value = -1
-# xvaluerecord = 0
-# yvaluerecord = 0
-# totalvalues = 0
-# for l in range(21):
-#     for i in range(number_of_entries):
-#         if xcoord[i] == round(value, 1):
-#             xvaluerecord = xvaluerecord + 1
-#         if ycoord[i] == round(value, 1):
-#             yvaluerecord = yvaluerecord + 1
-#     print("x" + str(value) + " " +str(xvaluerecord))
-#     print("y" + str(value) + " " + str(yvaluerecord))
-#     value = round(value, 1) + 0.1
-#     totalvalues += xvaluerecord + yvaluerecord
-#     xvaluerecord = 0
-#     yvaluerecord = 0
-#print(totalvalues)
-
-# Screening end results
-# print(ympyran_ulkopuolella)
-# print(ympyran_sisalla)
-#
-# Testing lists.
-# for x in xcoord:
-#    print("X coord" + str(xcoord[ind]),end="")
-#    print("Y coord" + str(ycoord[ind]))
-#    ind = ind + 1
