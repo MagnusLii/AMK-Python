2aa8febc655fcd1f2a45d144e59ff8b356bfcc56
Mage <magnus.liivamagi@gmail.com>
2022-08-31 16:34:30 +0300

    Moved troubleshooting code from 4.6 to separate file.

---
+++ Tehtavat/Moduuli_06/Troubleshoot code.py
@@ -0,0 +1,28 @@
+# Troubleshooting code.
+# value = -1
+# xvaluerecord = 0
+# yvaluerecord = 0
+# totalvalues = 0
+# for l in range(21):
+#     for i in range(number_of_entries):
+#         if xcoord[i] == round(value, 1):
+#             xvaluerecord = xvaluerecord + 1
+#         if ycoord[i] == round(value, 1):
+#             yvaluerecord = yvaluerecord + 1
+#     print("x" + str(value) + " " +str(xvaluerecord))
+#     print("y" + str(value) + " " + str(yvaluerecord))
+#     value = round(value, 1) + 0.1
+#     totalvalues += xvaluerecord + yvaluerecord
+#     xvaluerecord = 0
+#     yvaluerecord = 0
+# print(totalvalues)
+# print()
+# #Screening end results
+# print(outside_of_circle)
+# print(inside_of_circle)
+#
+# Testing lists.
+# for x in xcoord:
+#    print("X coord" + str(xcoord[ind]),end="")
+#    print("Y coord" + str(ycoord[ind]))
+#    ind = ind + 1
