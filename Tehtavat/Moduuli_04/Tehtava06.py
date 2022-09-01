87011a2f05a5ca734296a25af5035c54c77e3d39
Mage <magnus.liivamagi@gmail.com>
2022-09-01 15:43:41 +0300

    Updated 4.6

--- Tehtavat/Moduuli_04/Tehtava06.py
+++ Tehtavat/Moduuli_04/Tehtava06.py
@@ -1,13 +1,9 @@
 import random
 
 # Var
-xcoord = []                 # Lists for storing all X/Y coordinates.
-ycoord = []
-ind = 0                     # Var for keeping track of which item were reading on the list.
-number_of_entries = 0       # Variable for storing the number of numbers asked to be generated.
-inside_of_circle = 0        # These two are variables used to keep track of the...
-outside_of_circle = 0       # ratio of numbers within and outside the circle.
-
+ind = 0
+number_of_entries = 0
+inside_of_circle = 0
 # Code
 # Number of coordinates generated.
 while number_of_entries == 0:
@@ -16,22 +12,16 @@ while number_of_entries == 0:
     except ValueError:
         print("Syötä kokonaisnumero.")
 
-# Generating coordinates.
-for i in range(number_of_entries):
-    xcoord.append(round(random.uniform(-1.003, 1.001), 1))
-    ycoord.append(round(random.uniform(-1.003, 1.001), 1))
-
 # Evaluating positions.
 for i in range(number_of_entries):
-    if (xcoord[ind] ** 2) + (ycoord[ind] ** 2) < 1:
+    xcor = random.uniform(-1, 1)
+    ycor = random.uniform(-1, 1)
+    if (xcor ** 2) + (ycor ** 2) < 1:
         inside_of_circle += 1
-        ind += 1
-    else:
-        outside_of_circle += 1
         ind += 1
 
 # Calculating pi
 try:
-    print(inside_of_circle / outside_of_circle)
+    print(4*inside_of_circle / number_of_entries)
 except ZeroDivisionError:
     print("Kaikki pisteet osuivat ympyrän sisälle.")
