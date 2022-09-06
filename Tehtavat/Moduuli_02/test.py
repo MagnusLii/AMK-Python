6f3cb29c95c4d38fa091b6f4d1a4d977e189bd13
Mage <magnus.liivamagi@gmail.com>
2022-09-06 10:35:44 +0300

    PEP8:ted 6.4

--- Tehtavat/Moduuli_02/test.py
+++ Tehtavat/Moduuli_02/test.py
@@ -1,4 +1,18 @@
-x1 = input("float plz")
+leiviskät = input('anna leiviskät: ')
+naulat = input('anna naulat: ')
+luodit = input('anna luodit: ')
 
-type(x1)
-print(x1)
+luoti = 13.3
+naula = 32*luoti
+leiviskä = 20*naula
+
+luotipaino = luoti*float(luodit)
+naulapaino = naula*float(naulat)
+leiviskäpaino = leiviskä*float(leiviskät)
+
+grammatyheensä = luotipaino+naulapaino+leiviskäpaino
+
+kiloiks = grammatyheensä//1000
+grammat = f'{(grammatyheensä/1000-kiloiks)*1000:.2f}'
+
+print(f'Massa nykymittojen mukaan: {int(kiloiks)} kilogrammaa ja {grammat} grammaa. ')
