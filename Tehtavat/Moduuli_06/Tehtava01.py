233c2a46c6c623b8831bb891ca48aca42b1d8683
Mage <magnus.liivamagi@gmail.com>
2022-09-06 10:25:56 +0300

    rolled back changes to 6.1

--- Tehtavat/Moduuli_06/Tehtava01.py
+++ Tehtavat/Moduuli_06/Tehtava01.py
@@ -2,8 +2,14 @@ import random
 
 roll = None
 
-while roll != 6:
+
+def dicethrow():
+    global roll
     roll = random.randint(1, 6)
+
+
+while roll != 6:
+    dicethrow()
     print(roll)
     if roll == 6:
         exit()
