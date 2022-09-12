60ad950d3053c59581b9230c7d2d5f8cc7773eb8
Mage <magnus.liivamagi@gmail.com>
2022-09-12 22:09:39 +0300

    Added teht 8.1

---
+++ Tehtavat/Moduuli_08/Tehtava_01.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+sqlconnect = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    database="flight_game1",
+    user="root",
+    password="5579",
+    autocommit=True
+)
+
+
+def getICAOinf(icao):
+    sql = f'''SELECT name, municipality
+      FROM airport
+      WHERE ident = "{icao}"'''
+    #print(sql)
+    cursor = sqlconnect.cursor()
+    cursor.execute(sql)
+    outcome = cursor.fetchall()
+    print(outcome)
+
+getICAOinf(input("Anna ICAO: ").upper())
