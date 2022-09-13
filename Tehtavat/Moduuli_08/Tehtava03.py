e5f22f1e9b4958158b3ed08ee972c98bbb74aa50
Mage <magnus.liivamagi@gmail.com>
2022-09-13 18:05:43 +0300

    Updated 8.3

--- Tehtavat/Moduuli_08/Tehtava03.py
+++ Tehtavat/Moduuli_08/Tehtava03.py
@@ -3,8 +3,6 @@
 # Löytyy myös tästä AO linkistään.
 # https://github.com/MagnusLii/ohjelmisto1/blob/master/Tehtavat/Moduuli_08/SQLfunctions.py
 
-# TODO: update import model.
-from geopy.distance import geodesic as GD
 import geopy.distance
 
 coords = []
@@ -21,15 +19,6 @@ for i in range(2):
     userinp = (input(f"Give ICAO{i+1}: ").upper())
     coords.append(getcoords(userinp))
 
-
-#icao1 = (input("Give ICAO1: ").upper())
-#icao2 = (input("Give ICAO2: ").upper())
-#icao1 = "AYMN" # (-6.14774, 143.656998)
-#icao2 = "EFHK" # (60.3172, 24.963301)
-
-#coords.append(getcoords(icao1))
-#coords.append(getcoords(icao2))
-
 x = 0
 for i in coords:
     item = coords[x]
@@ -40,8 +29,4 @@ for i in coords:
     coords[x] = item
     x += 1
 
-#print(coords[0])
-#print(coords[1])
-#print(coords)
-#print(GD((-6.14774, 143.656998), (60.3172, 24.963301)))
 print(f"The distance between the specified airports is {geopy.distance.geodesic(coords[0], coords[1])}")
