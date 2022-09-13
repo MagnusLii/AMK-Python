3bd09bae4fcaa6ed85810477e087d558cd73012f
Mage <magnus.liivamagi@gmail.com>
2022-09-13 13:03:25 +0300

    Updated SQLfun

--- Tehtavat/Moduuli_08/SQLfunctions.py
+++ Tehtavat/Moduuli_08/SQLfunctions.py
@@ -10,13 +10,9 @@ sqlconnect = mysql.connector.connect(
     autocommit=True
 )
 
-def sqlGetPlayers():
-    sql = "SELECT id, co2_consumed, co2_budget," \
-          " location, screen_name FROM game"
-    print(sql)
+
+def cursor(input):
     cursor = sqlconnect.cursor()
-    cursor.execute(sql)
+    cursor.execute(input)
     outcome = cursor.fetchall()
-    if cursor.rowcount > 0:
-        print(outcome)
-    return
+    return outcome
