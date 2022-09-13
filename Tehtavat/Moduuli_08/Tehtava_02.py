928b97cb0d1c4364ca62fd4f321b0b6633468e63
Mage <magnus.liivamagi@gmail.com>
2022-09-13 09:26:06 +0300

    Added 8.2

---
+++ Tehtavat/Moduuli_08/Tehtava_02.py
@@ -0,0 +1,20 @@
+
+# Ohjelma hyödyntää komentoja/asetuksia Moduuli8/SQLfunctions.py tiedostostaan.
+# Löytyy myös tästä AO linkistään.
+# https://github.com/MagnusLii/ohjelmisto1/blob/master/Tehtavat/Moduuli_08/SQLfunctions.py
+
+from SQLfunctions import sqlconnect
+
+def getairporttype(country):
+    query = f'''SELECT type, COUNT(type)
+        FROM airport
+        WHERE iso_country = "{country}"
+        GROUP BY type
+        HAVING COUNT(type) > 1
+        ; '''
+    cursor = sqlconnect.cursor()
+    cursor.execute(query)
+    outcome = cursor.fetchall()
+    return outcome
+
+print(getairporttype(input("Anna maan koodi: ".upper())))
