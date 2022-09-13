3a708c55ae5016c32abc043d320f8e85db015852
Mage <magnus.liivamagi@gmail.com>
2022-09-13 13:24:09 +0300

    Korjattu kirjoitusvirheitä.

--- Tehtavat/Moduuli_06/Mallikoodi.py
+++ Tehtavat/Moduuli_06/Mallikoodi.py
@@ -3,7 +3,7 @@
 # pizzan yksikköhinnan euroina per neliömetri. Pääohjelma kysyy käyttäjältä
 # kahden pizzan halkaisijat ja hinnat sekä ilmoittaa, kumpi pizza antaa
 # paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
-# Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+# Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.l
 
 import math
 meemit = ["ovat", "tärkeitä"]
