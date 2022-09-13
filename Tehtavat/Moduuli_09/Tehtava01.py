aea44bfe3ab02d07c3680c916fbae65ffa3d5eae
Mage <magnus.liivamagi@gmail.com>
2022-09-14 00:38:10 +0300

    Added 9.1

---
+++ Tehtavat/Moduuli_09/Tehtava01.py
@@ -0,0 +1,12 @@
+
+class Car:
+    def __init__(self, registration_number, top_speed):
+        self.registration_number = registration_number
+        self.top_speed = top_speed
+        self.wheels = 0
+        self.distance_traveled = 0
+
+car1 = Car("ABC-420", 69)
+
+attributes = vars(car1)
+print(", ".join("%s: %s" % item for item in attributes.items()))
