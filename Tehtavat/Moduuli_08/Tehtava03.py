77a18399624c4e6848e0b431abd5e320028ca51c
Mage <magnus.liivamagi@gmail.com>
2022-09-13 09:39:30 +0300

    Updated 8.3

---
+++ Tehtavat/Moduuli_08/Tehtava03.py
@@ -0,0 +1,7 @@
+
+# Ohjelma hyödyntää komentoja/asetuksia Moduuli8/SQLfunctions.py tiedostostaan.
+# Löytyy myös tästä AO linkistään.
+# https://github.com/MagnusLii/ohjelmisto1/blob/master/Tehtavat/Moduuli_08/SQLfunctions.py
+
+from SQLfunctions import sqlconnect
+
