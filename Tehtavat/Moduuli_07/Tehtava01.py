4ac7c42680599948a21a1a06a01d66e50982d7a0
Mage <magnus.liivamagi@gmail.com>
2022-09-14 00:06:05 +0300

    Updated 7.1

--- Tehtavat/Moduuli_07/Tehtava01.py
+++ Tehtavat/Moduuli_07/Tehtava01.py
@@ -1,5 +1,5 @@
-vuodenaika = ("Talvi", "Talvi","Kevät",
-              "kevät", "Kevät","Kesä",
-              "Kesä","Kesä","Syksy",
-              "Syksy","Syksy","Talvi")
+vuodenaika = ("Talvi", "Talvi", "Kevät",
+              "kevät", "Kevät", "Kesä",
+              "Kesä", "Kesä", "Syksy",
+              "Syksy", "Syksy", "Talvi")
 print(vuodenaika[int(input("Anna kk numero: "))-1])
