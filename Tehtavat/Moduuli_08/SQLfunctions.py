611b2278f2e162ed20368c5f24b58f3351c34561
Mage <magnus.liivamagi@gmail.com>
2022-09-13 08:35:55 +0300

    SQLfun

--- Tehtavat/Moduuli_08/SQLfunctions.py
+++ Tehtavat/Moduuli_08/SQLfunctions.py
@@ -1,3 +1,13 @@
+
+sqlconnect = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    database="flight_game1",
+    user="root",
+    password="5579",
+    autocommit=True
+)
+
 def sqlGetPlayers():
     sql = "SELECT id, co2_consumed, co2_budget," \
           " location, screen_name FROM game"
