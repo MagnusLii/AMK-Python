c9c7119426d4879f147a3a4c86f3d5920b12c666
Mage <magnus.liivamagi@gmail.com>
2022-09-13 13:11:28 +0300

    Updated 8.3

--- Tehtavat/Moduuli_08/Tehtava03.py
+++ Tehtavat/Moduuli_08/Tehtava03.py
@@ -3,12 +3,17 @@
 # Löytyy myös tästä AO linkistään.
 # https://github.com/MagnusLii/ohjelmisto1/blob/master/Tehtavat/Moduuli_08/SQLfunctions.py
 
-from SQLfunctions import sqlconnect
+from SQLfunctions import cursor
 
 def getcoords(ICAO):
     query = f'''SELECT latitude_deg, longitude_deg 
         FROM airport WHERE ident = "{ICAO}"'''
+    return cursor(query)
 
 icao1 = input("Give ICAO1: ").upper()
 icao2 = input("Give ICAO2: ").upper()
+
+
+
+
 
