0c0ed6e091b4815b64ca21a4d661ae6e6430953f
Mage <magnus.liivamagi@gmail.com>
2022-09-16 09:18:08 +0300

    Fixed typo in 9.1

--- Tehtavat/Moduuli_09/Tehtava01.py
+++ Tehtavat/Moduuli_09/Tehtava01.py
@@ -3,7 +3,7 @@ class Car:
     def __init__(self, registration_number, top_speed):
         self.registration_number = registration_number
         self.top_speed = top_speed
-        self.wheels = 0
+        self.current_speed = 0
         self.distance_traveled = 0
 
 car1 = Car("ABC-420", 69)
